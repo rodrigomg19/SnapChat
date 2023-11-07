ea6b81286d55ba6b06ddf6262e7d37946aca4fd9
rodrigomg19 <rodrigo.ramos.d@tecsup.edu.pe>
2023-11-07 18:55:46 -0500

    Iniciar sesion

--- RamosSnapchat/ViewController.swift
+++ RamosSnapchat/ViewController.swift
@@ -6,9 +6,24 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class ViewController: UIViewController {
+class iniciarSesionViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {(user, error) in
+        print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            } else {
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
